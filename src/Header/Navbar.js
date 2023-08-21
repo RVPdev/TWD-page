43e7cafef1e1b61b4ba356e2083ea82d9f87f281
RenatoDevi <renatovilcahuaman@gmail.com>
2023-08-21 11:32:17 -0500

    pending commit

--- src/Header/Navbar.js
+++ src/Header/Navbar.js
@@ -7,19 +7,19 @@ function Navbar() {
     <nav className="shadow-md w-full fixed top-0 left-0">
       <div className="md:flex items-center justify-between bg-secondaryDark py-4 md:px-10 px7">
         <div className="font-bold text-2x1 cursor-pointer flex items-center font-[Poppins] text-primaryText">
-          TWD
+          TWD Buyers
         </div>
-        <ul className="text-secondaryText md:flex md:items-center">
-          <li className="md:ml-8 text-l hover:text-secondarySubtext duration-500">
+        <ul className="text-secondaryText md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-secondaryDark">
+          <li className="md:ml-8 text-l hover:text-secondarySubtext duration-500 md:m-0 my-7">
             <Link to="/">Home</Link>
           </li>
-          <li className="md:ml-8 text-l hover:text-secondarySubtext duration-500">
+          <li className="md:ml-8 text-l hover:text-secondarySubtext duration-500 md:m-0 my-7">
             <Link to="/Collection">Collection</Link>
           </li>
-          <li className="md:ml-8 text-l hover:text-secondarySubtext duration-500">
+          <li className="md:ml-8 text-l hover:text-secondarySubtext duration-500 md:m-0 my-7">
             <Link to="/About">About</Link>
           </li>
-          <li className="md:ml-8 text-l hover:text-secondarySubtext duration-500">
+          <li className="md:ml-8 text-l hover:text-secondarySubtext duration-500 md:m-0 my-7">
             <Link to="/Contact">Contact</Link>
           </li>
         </ul>
